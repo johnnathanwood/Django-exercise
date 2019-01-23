5eb57688617bee4e38f561c0e06705f0dd40b0ba
John Wood <johnnathanwood@gmail.com>
2019-01-23 12:58:41 -0600

    working with django

---
+++ cohorts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path 
+
+from . import views 
+
+app_name = 'cohorts'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:cohort_id>/', views.detail, name='detail'),
+]
